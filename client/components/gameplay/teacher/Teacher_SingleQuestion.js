aa761e1bc4f45eca36c65e65653c6b559455507d
Courtney Napleton <courtney.napleton@gmail.com>
2018-01-26 15:12:48 -0600

    changed backend route

--- client/components/gameplay/teacher/Teacher_SingleQuestion.js
+++ client/components/gameplay/teacher/Teacher_SingleQuestion.js
@@ -2,19 +2,21 @@ import firebase from '../../../../server/firebase'
 import React, { Component } from 'react'
 import { Card } from 'semantic-ui-react'
 import { shuffle } from '../../../utils'
-import { history} from '../../../history'
+import { history } from '../../../history'
 import { Redirect } from 'react-router-dom'
+import { setGameOnStateThunk } from '../../../store'
+import { connect } from 'react-redux'
 
 
 
-export default class TeacherSingleQuestion extends Component {
-  constructor(){
+export class TeacherSingleQuestion extends Component {
+  constructor() {
     super();
     this.state = {
       currentQuestion: {},
       timer: null,
       counter: 20,
-      answerArray: [],
+      shuffledArray: [],
       nextQuestionId: null
     }
     this.tick = this.tick.bind(this);
@@ -22,88 +24,89 @@ export default class TeacherSingleQuestion extends Component {
 
   }
 
-  async componentDidMount(){
+componentDidMount() {
 
     const gameRoomId = this.props.match.params.pin;
     const questionId = this.props.match.params.questionId;
     const nextQuestionId = +this.props.match.params.questionId + 1
+    this.props.setGameOnStateThunk(gameRoomId)
+    const currentGame = this.props.currentGame
+    console.log('currentGameObj', this.props.currentGame)
 
-    try {
-      await firebase.database().ref(`gameRooms/${gameRoomId}/quiz/${questionId}`)
-      .once('value', snapshot => {
-        const currentQuestion = snapshot.val();
-        console.log('currentquestion: ', currentQuestion);
-        const indexArray = Object.keys(currentQuestion.wrongAnswers);
-        const rightAnswer = currentQuestion.rightAnswer;
-        const answerArray = indexArray.map(index => {
-          return currentQuestion.wrongAnswers[index]
-        });
-        answerArray.push(rightAnswer);
-        const finalArray = shuffle(answerArray);
-
-        let timer = setInterval(this.tick, 1000);
-        this.setState({currentQuestion, answerArray: finalArray, timer, nextQuestionId});
-
-      })
-    } catch (err) {
-      console.error('error retrieving quiz: ', err);
-    }
-
+    let timer = setInterval(this.tick, 1000);
+    this.setState({ timer, nextQuestionId });
 
   }
 
-  // componentWillUnmount() {
-  //   this.setState({timer: null, counter: 20, nextQuestionId: null})
+// componentWillUnmount() {
+//   this.setState({timer: null, counter: 20, nextQuestionId: null})
 
-  // }
+// }
 
-
-  tick(){
-    if(this.state.counter === 0){
-      this.setState({counter: 20})
-    } else {
-    this.setState({counter: this.state.counter - 1});
-    }
+tick(){
+  if (this.state.counter === 0) {
+    this.setState({ counter: 20 })
+  } else {
+    this.setState({ counter: this.state.counter - 1 });
   }
+}
 
-  render() {
-    const question = this.state.currentQuestion;
-    const answerArray = this.state.answerArray;
-    const gameRoomId = this.props.match.params.pin;
-    const nextQuestionId = this.state.nextQuestionId
-    console.log('nextq', nextQuestionId)
-    const timer = this.state.counter;
-    console.log('timer', timer)
+shuffleToState(arr){
+  const questionId = this.props.match.params.questionId;
+
+}
+
+render() {
+  const gameRoomId = this.props.match.params.pin;
+  const questionId = this.props.match.params.questionId;
+  const nextQuestionId = this.state.nextQuestionId
+  const timer = this.state.counter;
+  const currentQuestion = this.props.currentGame[questionId]
+    console.log('currentq', currentQuestion)
+    const indexArray = currentQuestion && Object.keys(currentQuestion.answers);
+    const answerArray = indexArray && indexArray.map(index => {
+      return currentQuestion.answers[index]
+    });
 
 
-    return (
-      <div>
-        {
-          timer === 0
-          ? <Redirect to={`/teacher/${gameRoomId}/question/${nextQuestionId}`}/>
+
+  return (
+    <div>
+      {
+        timer === 0
+          ? <Redirect to={`/teacher/${gameRoomId}/question/${nextQuestionId}`} />
           :
-         (
-          <div>
+          (
             <div>
-              <Card>
-                <h1 id="teacher-single-question">{question.question && question.question}</h1>
-              </Card>
-              <Card.Group itemsPerRow={2}>
-                 <Card className="teacher-single-answer">{answerArray.length && answerArray[0]}</Card>
-                 <Card className="teacher-single-answer">{answerArray.length && answerArray[1]}</Card>
-                 <Card className="teacher-single-answer">{answerArray.length && answerArray[2]}</Card>
-                 <Card className="teacher-single-answer">{answerArray.length && answerArray[3]}</Card>
-               </Card.Group>
+              <div>
+                <Card>
+                  <h1 id="teacher-single-question">{currentQuestion && currentQuestion.question}</h1>
+                </Card>
+                <Card.Group itemsPerRow={2}>
+                  <Card className="teacher-single-answer">{answerArray.length && answerArray[0]}</Card>
+                  <Card className="teacher-single-answer">{answerArray.length && answerArray[1]}</Card>
+                  <Card className="teacher-single-answer">{answerArray.length && answerArray[2]}</Card>
+                  <Card className="teacher-single-answer">{answerArray.length && answerArray[3]}</Card>
+                </Card.Group>
               </div>
               <div>
                 <div>Time Remaining: {this.state.counter}</div>
               </div>
-          </div>
-        )
-        }
-      </div>
-    )
-  }
+            </div>
+          )
+      }
+    </div>
+  )
+}
 
 
 }
+
+const mapState = state => {
+  return {
+    currentGame: state.currentGame
+  }
+}
+const mapDispatch = { setGameOnStateThunk }
+
+export default connect(mapState, mapDispatch)(TeacherSingleQuestion)
--- client/store/currentGame.js
+++ client/store/currentGame.js
@@ -2,19 +2,30 @@ import history from '../history'
 import firebase from '../../server/firebase'
 
 
-const SET_GAME_ID = 'SET_GAME_ID'
+const SET_GAME_ON_STATE = 'SET_GAME_ON_STATE'
 
 
 
 
-export const setGameId = id => ({type: SET_GAME_ID, id})
+export const setGameOnState = game => ({type: SET_GAME_ON_STATE, game})
 
 
-export default function (currentGameId = '', action) {
+export const setGameOnStateThunk = (id) => dispatch =>{
+  firebase.database().ref(`gameRooms/${id}/quiz`)
+      .once('value', snapshot => {
+        const currentGame = snapshot.val();
+        dispatch(setGameOnState(currentGame))
+        });
+
+}
+
+
+
+export default function (currentGame = {}, action) {
   switch (action.type) {
-    case SET_GAME_ID:
-    return action.id
+    case SET_GAME_ON_STATE:
+    return action.game
     default:
-      return currentGameId
+      return currentGame
   }
 }
