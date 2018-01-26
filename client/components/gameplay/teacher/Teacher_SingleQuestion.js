43039bdab01d62882c27183baabb4ef985af8ca1
lcbuchanan <buchanan.lori@gmail.com>
2018-01-26 12:04:04 -0600

    teacher question view shows question and asnwers'

--- client/components/gameplay/teacher/Teacher_SingleQuestion.js
+++ client/components/gameplay/teacher/Teacher_SingleQuestion.js
@@ -1,25 +1,49 @@
 import firebase from '../../../../server/firebase'
 import React, { Component } from 'react'
-import { Form, Card, Button } from 'semantic-ui-react'
+import { Card } from 'semantic-ui-react'
+import { shuffle } from '../../../utils'
+import { history} from '../../../history'
 
 
 export default class TeacherSingleQuestion extends Component {
   constructor(){
     super();
     this.state = {
-      currentQuestion : {},
+      currentQuestion: {},
       timer: null,
-      counter: 120
+      counter: 120,
+      answerArray: []
     }
     this.tick = this.tick.bind(this);
   }
 
-  componentDidMount(){
-    //pull question object from firebase
-    //shuffle answer order
-    //start a timer
-    let timer = setInterval(this.tick, 1000);
-    this.setState({timer});
+  async componentDidMount(){
+
+    const gameRoomId = this.props.match.params.pin;
+    const questionId = this.props.match.params.questionId;
+
+    try {
+      await firebase.database().ref(`gameRooms/${gameRoomId}/quiz/${questionId}`)
+      .once('value', snapshot => {
+        const currentQuestion = snapshot.val();
+        console.log('currentquestion: ', currentQuestion);
+        const indexArray = Object.keys(currentQuestion.wrongAnswers);
+        const rightAnswer = currentQuestion.rightAnswer;
+        const answerArray = indexArray.map(index => {
+          return currentQuestion.wrongAnswers[index]
+        });
+        answerArray.push(rightAnswer);
+        const finalArray = shuffle(answerArray);
+
+        let timer = setInterval(this.tick, 1000);
+        this.setState({currentQuestion, answerArray: finalArray, timer});
+
+      })
+    } catch (err) {
+      console.error('error retrieving quiz: ', err);
+    }
+
+
   }
 
   componentWillUnmount() {
@@ -34,22 +58,41 @@ export default class TeacherSingleQuestion extends Component {
 
   render() {
 
+    // console.log('from the render function: ', this.state)
+    const question = this.state.currentQuestion;
+
+    const answerArray = this.state.answerArray;
+
+    const nextQuestionId = +this.props.match.params.questionId + 1;
+
+    const timer = this.state.timer;
+
+
     return (
       <div>
-        <div>
-          <Card>
-            <h1 id="teacher-single-question">This will be a question? </h1>
-          </Card>
-          <Card.Group>
-           <Card id="teacher-single-answer">Answer</Card>
-           <Card id="teacher-single-answer">AnotherAnswer</Card>
-           <Card id="teacher-single-answer">Nope not this</Card>
-           <Card id="teacher-single-answer">yes this one!</Card>
-           </Card.Group>
-        </div>
-        <div>
-          <div>Time Remaining: {this.state.counter}</div>
-        </div>
+        {
+          timer === 0
+          ? history.push(`/${nextQuestionId}`)
+          :
+         (
+          <div>
+            <div>
+              <Card>
+                <h1 id="teacher-single-question">{question.question && question.question}</h1>
+              </Card>
+              <Card.Group itemsPerRow={2}>
+                 <Card className="teacher-single-answer">{answerArray.length && answerArray[0]}</Card>
+                 <Card className="teacher-single-answer">{answerArray.length && answerArray[1]}</Card>
+                 <Card className="teacher-single-answer">{answerArray.length && answerArray[2]}</Card>
+                 <Card className="teacher-single-answer">{answerArray.length && answerArray[3]}</Card>
+               </Card.Group>
+              </div>
+              <div>
+                <div>Time Remaining: {this.state.counter}</div>
+              </div>
+          </div>
+        )
+        }
       </div>
     )
   }
