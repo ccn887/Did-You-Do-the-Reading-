0aae6251784410f26611c6c1995d1c73612b9e47
lcbuchanan <buchanan.lori@gmail.com>
2018-01-29 14:53:03 -0600

    stored some things in redux, but not everything

---
+++ client/store/currentStudents.js
@@ -0,0 +1,52 @@
+import history from '../history'
+import firebase from '../../server/firebase'
+
+
+/*Actions */
+const SET_CURRENT_STUDENT = 'SET_CURRENT_STUDENT'
+const UPDATE_STUDENT_LIST = 'UPDATE_STUDENT_LIST'
+
+/* Action creators */
+export const setCurrentStudent = id => ({type: SET_CURRENT_STUDENT, id})
+export const updateStudentList = list => ({type: UPDATE_STUDENT_LIST, list})
+
+
+
+
+/* thunk helpers */
+
+const database = firebase.database()
+const activeListeners = {}
+
+
+  /*Thunks*/
+export const listenForNewStudents =  (gameId) => (dispatch) => {
+   const path = `gameRooms/${gameId}/users`
+   const ref = database.ref(path)
+   const listener = (snapshot) => {
+     dispatch(updateStudentList(snapshot.val()))
+   }
+   activeListeners[path] = { ref, listener }
+   ref.on('value', listener)
+ }
+
+
+
+ export const stopListeningForNewStudents = (gameId) => (dispatch) => {
+   const path = `gameRooms/${gameId}/users`
+   const { ref, listener } = activeListeners[path]
+   ref.off('value', listener)
+   delete activeListeners[path]
+ }
+
+
+ /* Reducer */
+
+ export default function (list = {}, action) {
+    switch (action.type) {
+     case UPDATE_STUDENT_LIST:
+       return action.list
+      default:
+       return list
+    }
+  }
--- server/firebase.js
+++ server/firebase.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase'
 
-const API_KEY = process.env.WORDS_API_KEY
+const API_KEY = process.env.FIREBASE_API_KEY
 
 var config = {
-  apiKey: API_KEY,
+  apiKey: "AIzaSyCwlnC_PyCan4hJZD93I0u7cgOtOrkV-6A",
   authDomain: "did-you-do-the-reading.firebaseapp.com",
   databaseURL: "https://did-you-do-the-reading.firebaseio.com",
   projectId: "did-you-do-the-reading",
