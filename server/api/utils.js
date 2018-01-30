2c2cbe05b32c42d27df7443412c120c07c43a5b8
lcbuchanan <buchanan.lori@gmail.com>
2018-01-30 16:33:20 -0600

    stop 500 errors on backend, add failsafe for expired api key

--- server/api/utils.js
+++ server/api/utils.js
@@ -1,19 +1,31 @@
 const axios = require('axios');
 
 const API_KEY = process.env.WORDS_API_KEY;
+const API_KEY_BACKUP = process.env.BACKUP_WORDS_API_KEY;
 
 
 
 
 const lookup = async(word) => {
-  try{
-  const thesaurus = await axios.get(`http://words.bighugelabs.com/api/2/${API_KEY}/${word}/json`);
-  if (thesaurus.data.Error){
-    return undefined
-  }
-  return thesaurus.data;
+  
+  try {
+    const thesaurus = await axios.get(`http://words.bighugelabs.com/api/2/${API_KEY}/${word}/json`);
+    return thesaurus.data;
   }
   catch (err){
+    console.error('error from bighugelabs api: ', err.response.status, err.response.statusText);
+
+    if (err.response.status === 500 && err.response.statusText === 'Usage Exceeded'){
+        console.log('FIRST API KEY IS EXPIRED')
+      try {
+        const backupThesaurus = await axios.get(`http://words.bighugelabs.com/api/2/${API_KEY_BACKUP}/${word}/json`)
+        return backupThesaurus.data
+      }
+      catch (secondErr){
+        console.error('both our api keys are expired...', secondErr)
+        return undefined;
+      }
+    }
     return undefined
   }
 }
@@ -24,15 +36,22 @@ const getRandomIndex = (arr) => {
 }
 
 const getRandomWords = async(word, num) => {
-  const response = await axios.get(`http://api.datamuse.com/words?rel_trg=${word}`);
-  const objArray = response.data;
-  const firstIndex = getRandomIndex(objArray.slice(0));
-  const secondIndex = getRandomIndex(objArray.slice(0));
-  const firstWord = objArray[firstIndex].word;
-  const secondWord = objArray[secondIndex].word;
-  const thirdIndex = getRandomIndex(objArray.slice(0));
-  const thirdWord = objArray[thirdIndex].word;
-  return [firstWord, secondWord, thirdWord];
+  try {
+    const response = await axios.get(`http://api.datamuse.com/words?rel_trg=${word}`);
+    const objArray = response.data;
+
+
+    const firstIndex = getRandomIndex(objArray.slice(0));
+    const secondIndex = getRandomIndex(objArray.slice(0));
+    const firstWord = objArray[firstIndex].word || '';
+    const secondWord = objArray[secondIndex].word || '';
+    const thirdIndex = getRandomIndex(objArray.slice(0));
+    const thirdWord = objArray[thirdIndex].word;
+    return [firstWord, secondWord, thirdWord];
+  }
+  catch (err){
+    return undefined
+  }
 }
 
 const shuffle = (originalArray) =>{
