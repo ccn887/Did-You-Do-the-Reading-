c268148201c9230a239164294c3d9badf6885c20
Courtney Napleton <courtney.napleton@gmail.com>
2018-02-01 11:46:01 -0600

    fixed weird errors

--- server/api/utils.js
+++ server/api/utils.js
@@ -63,9 +63,9 @@ const lookup = async(wordObj) => {
 
   }
   catch (err){
-    console.error('error from bighugelabs api: ', err.response.status, err.response.statusText);
+    console.error('error from bighugelabs api: ', err);
 
-    if (err.response.status === 500 && err.response.statusText === 'Usage Exceeded'){
+    if (err.response && err.response.status === 500 && err.response.statusText === 'Usage Exceeded'){
         console.log('FIRST API KEY IS EXPIRED')
       try {
         const backupThesaurus = await axios.get(`http://words.bighugelabs.com/api/2/${API_KEY_BACKUP}/${word}/json`)
