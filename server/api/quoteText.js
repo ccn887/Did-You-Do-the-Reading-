01579dbe77824b55b9f9adb4bd04a24ff34f7960
Sunil Sundar <sunilsundar2@gmail.com>
2018-02-01 16:55:54 -0600

    quotations questions enabled

--- server/api/quoteText.js
+++ server/api/quoteText.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { findSentiment, findPeople, findPlaces, findQuotations, quoteQuestions } = require('./quoteUtils');
+const { findPeople, findPlaces, findQuotations, quoteQuestions } = require('./quoteUtils');
 
 
 module.exports = router;
@@ -8,23 +8,13 @@ router.post('/quoteQuestion', (req, res, next) => {
  
   const text = `He didn't look at it, but after a moment he spoke to it. "How did you know it was me?" Courtney Napleton asked. Some sentence in between. "You aren't ever!"
   "You'd be stiff if you'd been sitting on a brick wall all day," said Professor McGonagall, Courtney Napleton, Harold Potter.` 
-// const quotations = findQuotations(text)
-const peopleArr = ["Jake", "Courtney Napleton", "Professor McGonagall" ]
-// console.log('string of quotes:', quotations)
-const question = quoteQuestions(text, "How did you know it was me?", peopleArr )
-
-    // const peopleResult = findPeople(text)
-    // const placesResult = findPlaces(text)
-    // console.log(placesResult)
-    // res.send(entityresult)
-    // console.log(question)
+const quotations = findQuotations(text)
+    const peopleResult = findPeople(text)
+    const placesResult = findPlaces(text)
+    const question = quotations.map(quote => {
+      return quoteQuestions(text,  quote.text, peopleResult )
+    })
     res.send(question)
 
 })
-
-
-//found quotes, use string.match to find the  ten longest quotes in text, if exist.  
-// see if ONE recognized persos in portion of proximate text.
-//if so, who said aforementioned quote, wrong answers are other entities found.  right answer is person found in proximal result.
-
 
--- server/api/quoteUtils.js
+++ server/api/quoteUtils.js
@@ -5,61 +5,32 @@ const GOOGLE_NAT_LANG_API_KEY = process.env.GOOGLE_NATURAL_LANGUAGE_API_KEY
 const nlp = require('compromise')
 
 
-// const people = findPeople(txt)
-
-var txt = `One morning I caught my foot in the crack where two old treetrunks
-joined. "Owp!" I yelled. "Mama! Waa!" I was out much later than I'd
-meant to be. As a rule I was back in the cave by dawn, but that day
-I'd been lured out farther than usual by the heavenly scent of newborn
-calf--ah, sweeter than flowers, as sweet as my mama's milk. I looked
-at the foot in anger and disbelief. It was wedged deep, as if the two
-oak trees were eating it. Black sawdust--squirreldust--was spattered
-up the leg almost to the thigh. I'm not sure now how the accident
-happened. I must have pushed the two boles apart as I stepped up into
-the place where they joined, and then when I stupidly let go again
-they closed on my foot like a trap. Blood gushed from my ankle and
-shin, and pain flew up through me like fire up the flue of a mountain.
-I lost my head. I bellowed for help, so loudly it made the ground
-shake. "Mama! Waa! Waaa!" I bellowed to the sky, the forest, the
-cliffs, until I was so weak from loss of blood I could barely wave my
-arms. "I'm going to die," I wailed. "Poor Grendel! Poor old Mama!" I
-wept and sobbed. "Poor Grendel will hang here and starve to death," I
-told myself, "and no one will ever even miss him!" The thought enraged
-me. I hooted. I thought of my mother's foreign eyes, staring at me
-from across the room: I thought of the cool, indifferent eyes of the
-others. I shrieked in fear; still no one came.`
-
-var quote = "Mama! Waa! Waaa!"
-
-
 function quoteQuestions( txt, quote, people){
 var sendQuoteObj = {}
+    const lowQuote = quote.toLowerCase()
+    const lowTxt = txt.toLowerCase()
   const qLength = quote.length
-  const startIndex = txt.indexOf(quote)+qLength+1
+  const startIndex = lowTxt.indexOf(lowQuote)+qLength+1
   let residue = ''
   
-  for (let i = startIndex; i < txt.length; i++){
+  for (let i = startIndex; i < lowTxt.length; i++){
     
-    if(txt[i-1] === "."){
+    if(lowTxt[i-1] === "."){
     break;
     }
-    residue += txt[i]
+    residue += lowTxt[i]
   }
   const peopleFiltered = people.filter(item => {
-      console.log('peoplefiltereditem', item)
     return residue.includes(item)
   })
-  console.log('filter people', peopleFiltered)
   if (peopleFiltered.length === 1){
-    let question = "who said " + quote + "?'"
+    let question = `Who said, "` + quote + `"?`
     let rightAnswer = peopleFiltered[0]
     sendQuoteObj.question = question
     sendQuoteObj.rightAnswer = rightAnswer
   }
   return sendQuoteObj
 }
-
-
 
 const findSentiment = async(text)=> {
   try{
@@ -73,13 +44,11 @@ const findSentiment = async(text)=> {
 
 
 const findPeople = (quoteStr) => {
-      const doc = nlp(quoteStr)
-      const people = doc.people()
-   
-      people.sort('frequency').unique()
-      const peopleArr =  people.out('freq')
-      console.log('thereAL people shady', peopleArr)
-      return peopleArr
+    const doc = nlp(quoteStr)
+    const people = doc.people()
+    people.normalize().sort('frequency').unique()
+    const peopleArr =  people.out('array')
+    return peopleArr
     }
 
     const findPlaces = (quoteStr) => {
@@ -93,34 +62,7 @@ const findPeople = (quoteStr) => {
   const findQuotations = (quoteStr) => {
     const doc = nlp(quoteStr)
     const quotations = doc.quotations()
- 
-    // people.sort('frequency').unique()
-    // const peopleArr =  people.out('array')
     return quotations.data()
   }
-
-
-// const findEntities = async (quoteStr) => {
-//   try{
-//     const doc = nlp(quoteStr)
-//     console.log('docPeople', doc)
-//     const people = doc.people()
- 
-//     people.normalize().sort('frequency').unique()
-//     const peopleArr =  people.out('array')
-
-//     const places = doc.places()
-//     places.sort('alpha')
-//    const placesArr =  places.out('array'))
-
-
-    
-//     // doc.sentences().toNegative()
-//     // return entities
-//   }
-//   catch (err){
-//     console.log(err)
-//   }
-// }
 
 module.exports = {findSentiment, findPlaces, findPeople, findQuotations, quoteQuestions}
