4e73c62970719f21e99bcd5da5b64eb3e5ee25ce
lcbuchanan <buchanan.lori@gmail.com>
2018-02-03 14:27:55 -0600

    add individual student graphs

--- client/components/gameplay/teacher/AllStudentsGraph.js
+++ client/components/gameplay/teacher/AllStudentsGraph.js
@@ -1,80 +1,103 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
 import { VictoryLine, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
 import { Container } from 'semantic-ui-react'
-import { fetchAllStudentsWithScoreData } from '../../../store'
 
 
-export class AllStudentsGraph extends Component {
+export default function AllStudentsGraph(props){
+  const bigData = props.data
+
+  const idArray = props.studentIds
 
 
-  constructor(props){
-    super(props)
+  const dataOne = bigData[idArray[0]]
+
+  const dataTwo = bigData[idArray[1]]
+
+  const colors = [
+    "#114B5F",
+    "#88D498",
+    "#C6DABF",
+    "#008080",
+    "#F3E9D2",
+    "#FFA500",
+    "#663399"
+  ];
+
+  return (
+    <Container>
+    <h1 className="graph-title">All Students Scores </h1>
+        <VictoryChart domainPadding={20} theme={VictoryTheme.grayscale}>
+          <VictoryAxis label="Time (ms)" />
+          <VictoryAxis dependentAxis/>
+
+          {
+            idArray && idArray.map(studentId => {
+              return (
+                <VictoryLine
+                key={studentId}
+                data={bigData[studentId]}
+                x="date"
+                y="score"
+                />
+              )
+            })
+          }
+        </VictoryChart>
+    </Container>
+  )
   }
 
-  // componentDidMount(){
-  //   this.props.fetchAllStudentsWithScoreData()
+
+  // constructor(props){
+  //   super(props)
   // }
-
-
-
-  render(){
-
-
-    const bigData = this.props.data
-
-    const idArray = this.props.studentIds
-
-    console.log('data one ', bigData[idArray[0]])
-    console.log('data two ', bigData[idArray[1]])
-
-    const dataOne = bigData[idArray[0]]
-
-    const dataTwo = bigData[idArray[1]]
-
-    const colors = [
-      "#114B5F",
-      "#88D498",
-      "#C6DABF",
-      "#008080",
-      "#F3E9D2",
-      "#FFA500",
-      "#663399"
-    ];
-
-    return (
-      <Container>
-      <h1 className="graph-title">All Students Scores </h1>
-          <VictoryChart
-          domainPadding={20}
-          theme={VictoryTheme.grayscale}
-          >
-            <VictoryAxis label="Time (ms)" />
-            <VictoryAxis dependentAxis/>
-            <VictoryLine
-            data={dataOne}
-            x="time"
-            y="score"
-            />
-            <VictoryLine
-            data={dataTwo}
-            x="time"
-            y="score"
-            />
-          </VictoryChart>
-      </Container>
-    )
-  }
-
-}
-
-// const mapDispatch = {fetchAllStudentsWithScoreData}
-//
-// const mapState = state => {
-//   return {
-//     allStudentsGraphData: state.allStudentsGraphData
-//   }
-// }
-
-
-export default connect(null, null)(AllStudentsGraph)
+  //
+  //
+  //
+  // render(){
+  //
+  //
+  //   const bigData = this.props.data
+  //
+  //   const idArray = this.props.studentIds
+  //
+  //   console.log('data one ', bigData[idArray[0]])
+  //   console.log('data two ', bigData[idArray[1]])
+  //
+  //   const dataOne = bigData[idArray[0]]
+  //
+  //   const dataTwo = bigData[idArray[1]]
+  //
+  //   const colors = [
+  //     "#114B5F",
+  //     "#88D498",
+  //     "#C6DABF",
+  //     "#008080",
+  //     "#F3E9D2",
+  //     "#FFA500",
+  //     "#663399"
+  //   ];
+  //
+  //   return (
+  //     <Container>
+  //     <h1 className="graph-title">All Students Scores </h1>
+  //         <VictoryChart
+  //         domainPadding={20}
+  //         theme={VictoryTheme.grayscale}
+  //         >
+  //           <VictoryAxis label="Time (ms)" />
+  //           <VictoryAxis dependentAxis/>
+  //           <VictoryLine
+  //           data={dataOne}
+  //           x="time"
+  //           y="score"
+  //           />
+  //           <VictoryLine
+  //           data={dataTwo}
+  //           x="time"
+  //           y="score"
+  //           />
+  //         </VictoryChart>
+  //     </Container>
+  //   )
+  // }
