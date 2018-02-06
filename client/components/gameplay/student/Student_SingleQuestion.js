21f88713b5052ce2a3b707860c9d863780c77cfd
lcbuchanan <buchanan.lori@gmail.com>
2018-02-06 17:30:04 -0600

    show only two buttons to user if question is true.false

--- client/components/gameplay/student/Student_SingleQuestion.js
+++ client/components/gameplay/student/Student_SingleQuestion.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, Button } from 'semantic-ui-react'
+import { Card, Button, Container } from 'semantic-ui-react'
 import history from '../../../history'
 import { setGameOnStateThunk, setCurrentQuestionThunk, listenForGameStateChange, stopListeningForGameState, addToStudentStreak, breakStudentStreak, addToStudentScore, determineQuestionNumber } from '../../../store'
 import { connect } from 'react-redux'
@@ -76,10 +76,21 @@ export class StudentSingleQuestion extends Component {
             <Card>
               <h1 id="student-single-question">{currentQuestion && currentQuestion.question}</h1>
             </Card>
-            <Button value={answerArray[0]} onClick={this.submitAnswer} className="student-single-answer">{answerArray.length && answerArray[0]}</Button>
-            <Button value={answerArray[1]} onClick={this.submitAnswer} className="student-single-answer">{answerArray.length && answerArray[1]}</Button>
-            <Button value={answerArray[2]} onClick={this.submitAnswer} className="student-single-answer">{answerArray.length && answerArray[2]}</Button>
-            <Button value={answerArray[3]} onClick={this.submitAnswer} className="student-single-answer">{answerArray.length && answerArray[3]}</Button>
+            {
+              answerArray.length === 2
+              ?
+              <Container>
+                <Button value={answerArray[0]} onClick={this.submitAnswer} className="student-single-answer">{answerArray.length && answerArray[0]}</Button>
+                <Button value={answerArray[1]} onClick={this.submitAnswer} className="student-single-answer">{answerArray.length && answerArray[1]}</Button>
+              </Container>
+              :
+              <Container>
+                <Button value={answerArray[0]} onClick={this.submitAnswer} className="student-single-answer">{answerArray.length && answerArray[0]}</Button>
+                <Button value={answerArray[1]} onClick={this.submitAnswer} className="student-single-answer">{answerArray.length && answerArray[1]}</Button>
+                <Button value={answerArray[2]} onClick={this.submitAnswer} className="student-single-answer">{answerArray.length && answerArray[2]}</Button>
+                <Button value={answerArray[3]} onClick={this.submitAnswer} className="student-single-answer">{answerArray.length && answerArray[3]}</Button>
+              </Container>
+            }
           </div>
         </div>
       </div >
